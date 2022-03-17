fdea9af4c7d368a536347b5bf73802c1995a9023
Selena Li <lis24@up.edu>
2022-03-16 17:49:58 -0700

    Added method headers and completed testIsValid()

--- app/src/test/java/com/example/crazyeights_lis24_millemal22_sakata24_uyechi23/CrazyEights/CardTest.java
+++ app/src/test/java/com/example/crazyeights_lis24_millemal22_sakata24_uyechi23/CrazyEights/CardTest.java
@@ -6,6 +6,14 @@ import org.junit.Test;
 
 public class CardTest extends TestCase {
 
+    /**
+     * testSetValue
+     *
+     * Verifies that the value of the card based on it's face value is
+     * being set correctly
+     *
+     * @return void
+     */
     @Test
     public void testSetValue() {
         // general situation
@@ -14,6 +22,13 @@ public class CardTest extends TestCase {
         assertEquals(10, card1.value);
     }
 
+    /**
+     * testMatchSuit
+     *
+     * Verifies that cards with matching suits are being compared correctly
+     *
+     * @return void
+     */
     @Test
     public void testMatchSuit() {
         // general situation
@@ -26,6 +41,13 @@ public class CardTest extends TestCase {
         assertFalse(card3.matchSuit(card4));
     }
 
+    /**
+     * testMatchFace
+     *
+     * Verifies that cards with matching faces are being compared correctly
+     *
+     * @return void
+     */
     @Test
     public void testMatchFace() {
         // general situation
@@ -38,8 +60,22 @@ public class CardTest extends TestCase {
         assertFalse(card3.matchFace(card4));
     }
 
+    /**
+     * testIsValid
+     *
+     * Verifies that the two cards being played are valid
+     *
+     * @return void
+     */
     @Test
     public void testIsValid() {
-
+        // general situation
+        Card card1 = new Card("Queen", "Hearts");
+        Card card2 = new Card("Jack", "Hearts");
+        assertTrue(card1.isValid(card2));
+        // invalid face
+        Card card3 = new Card("King", "Diamonds");
+        Card card4 = new Card("Ace", "Spades");
+        assertFalse(card3.isValid(card4));
     }
 }
