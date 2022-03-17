702dea27e5ce9b45bd55267d377446d3c9a0a488
tundr <sakata24@up.edu>
2022-03-16 17:28:29 -0700

    3/16/22 5:27pm implemented test methods for three of the card methods

--- app/src/test/java/com/example/crazyeights_lis24_millemal22_sakata24_uyechi23/CrazyEights/CardTest.java
+++ app/src/test/java/com/example/crazyeights_lis24_millemal22_sakata24_uyechi23/CrazyEights/CardTest.java
@@ -2,17 +2,44 @@ package com.example.crazyeights_lis24_millemal22_sakata24_uyechi23.CrazyEights;
 
 import junit.framework.TestCase;
 
+import org.junit.Test;
+
 public class CardTest extends TestCase {
 
+    @Test
     public void testSetValue() {
+        // general situation
+        Card card1 = new Card("Queen", "Hearts");
+        card1.setValue("Queen");
+        assertEquals(10, card1.value);
     }
 
+    @Test
     public void testMatchSuit() {
+        // general situation
+        Card card1 = new Card("Queen", "Hearts");
+        Card card2 = new Card("Jack", "Hearts");
+        assertTrue(card1.matchSuit(card2));
+        // invalid suit
+        Card card3 = new Card("Queen", "Tuxedo ");
+        Card card4 = new Card("Jack", "Hearts");
+        assertFalse(card3.matchSuit(card4));
     }
 
+    @Test
     public void testMatchFace() {
+        // general situation
+        Card card1 = new Card("Queen", "Diamonds");
+        Card card2 = new Card("Queen", "Hearts");
+        assertTrue(card1.matchFace(card2));
+        // invalid face
+        Card card3 = new Card("adsf ", "Hearts");
+        Card card4 = new Card("Queen", "Hearts");
+        assertFalse(card3.matchFace(card4));
     }
 
+    @Test
     public void testIsValid() {
+
     }
 }
