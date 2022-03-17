68ce3f8268e0dea08391ad25311e353eae01933b
tundr <sakata24@up.edu>
2022-03-16 17:14:45 -0700

    3/16/22 5:14pm added the CardTest file for unit tests

---
+++ app/src/test/java/com/example/crazyeights_lis24_millemal22_sakata24_uyechi23/CrazyEights/CardTest.java
@@ -0,0 +1,18 @@
+package com.example.crazyeights_lis24_millemal22_sakata24_uyechi23.CrazyEights;
+
+import junit.framework.TestCase;
+
+public class CardTest extends TestCase {
+
+    public void testSetValue() {
+    }
+
+    public void testMatchSuit() {
+    }
+
+    public void testMatchFace() {
+    }
+
+    public void testIsValid() {
+    }
+}
